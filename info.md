03abb4200324715be00ae2bd1e9656559b66b5af
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2023-07-03 11:09:46 -0500

    Eventos

--- info.md
+++ info.md
@@ -10,3 +10,21 @@ pip install uvicorn
 
 ## Correr el servidor
 uvicorn main:app --reload
+
+
+## Documentacion
+Cuando trabajas con FastAPI se crea la documentacion automaticamente
+http://localhost:8000/docs
+
+## Eventos
+Se deben colocar decoradores para que se ejecuten los eventos
+
+@app.on_event("startup") Para que se ejecute al iniciar
+def startup():
+    print("El servidor va a comenzar")
+
+
+@app.on_event("shutdown")  Para que se ejecute al final 
+def shutdown():
+    print("El servidor se encuentra finalizando")
+
--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Proyecto para reseñar peliculas", descripion="En este proyecto seremos capaces de reseñar peliculas.", version="1")
 
+@app.on_event("startup")
+def startup():
+    print("El servidor va a comenzar")
+
+@app.on_event("shutdown")
+def shutdown():
+    print("El servidor se encuentra finalizando")
+
 @app.get("/")
 async def index():
     return "Hola mundo, desde servidor en FastAPI"
@@ -9,3 +17,4 @@ async def index():
 @app.get("/about")
 async def about():
     return "About"
+
