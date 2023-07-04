1a6d0b63c5ab5fe801becc2d0d6bb12ef1623add
jorgea-hn <jorgeahenriqueznovoa@gmail.com>
2023-07-03 21:32:01 -0500

    Validacion de score

--- schemas.py
+++ schemas.py
@@ -45,6 +45,13 @@ class ReviewRequestModel(BaseModel):
     review:str
     score:int
 
+    @validator('score')
+    def score_validator(cls,score):
+        if score < 1 or score>5:
+            raise ValueError("El rango para el score es de 1 a 5")
+        
+        return score
+
 class ReviewResponseModel(ResponseModel):
     id:int
     movie_id:int
